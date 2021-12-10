5a2bb5ff14e71eb2dbd01255078823271c9d3ef5
Liakot Ali Liton <liakotali2k18@gmail.com>
2021-12-10 20:06:57 +0600

    ActivityAddCustomer added

---
+++ app/src/main/java/com/liakot/easytransaction/ActivityAddCustomer.java
@@ -0,0 +1,14 @@
+package com.liakot.easytransaction;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityAddCustomer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_customer);
+    }
+}
