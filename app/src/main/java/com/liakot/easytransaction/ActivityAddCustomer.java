bd1d4ab73e14c82ab3a4413fadc459f2058ea709
Liakot Ali Liton <liakotali2k18@gmail.com>
2021-12-15 00:05:12 +0600

    add a dropdown menu in ActivityAddCustomer and customize AppTheme

--- app/src/main/java/com/liakot/easytransaction/ActivityAddCustomer.java
+++ app/src/main/java/com/liakot/easytransaction/ActivityAddCustomer.java
@@ -3,12 +3,26 @@ package com.liakot.easytransaction;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 
 public class ActivityAddCustomer extends AppCompatActivity {
+
+    AutoCompleteTextView customerType;
+    ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_customer);
+        customerType = findViewById(R.id.addCustomerType);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        adapter = new ArrayAdapter<String>(ActivityAddCustomer.this, R.layout.customer_type_dropdown_item, getResources().getStringArray(R.array.CustomerType));
+        customerType.setAdapter(adapter);
     }
 }
