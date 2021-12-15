1184334519833fe0f45019575f5a5fe46db7d631
Liakot Ali Liton <liakotali2k18@gmail.com>
2021-12-15 11:19:17 +0600

    Customize AcitiitySignUp page and connect with previous page

--- app/src/main/java/com/liakot/easytransaction/ActivitySignUp.java
+++ app/src/main/java/com/liakot/easytransaction/ActivitySignUp.java
@@ -7,19 +7,20 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-    Button homeBtn;
+public class ActivitySignUp extends AppCompatActivity {
+
+    Button signUpBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_signup);
 
-        homeBtn = findViewById(R.id.homeBtn);
-        homeBtn.setOnClickListener(new View.OnClickListener() {
+        signUpBtn = findViewById(R.id.signUpBtn);
+        signUpBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, ActivitySignUp.class));
+                startActivity(new Intent(ActivitySignUp.this, ActivityHome.class));
             }
         });
     }
