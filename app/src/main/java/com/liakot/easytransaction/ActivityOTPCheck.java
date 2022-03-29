e402cc00ce870ba37421abb98a302b7991f10e10
Liakot-ali <liakotali2k18@gmail.com>
2022-03-29 12:57:57 +0600

    phone vefication with firebase

---
+++ app/src/main/java/com/liakot/easytransaction/ActivityOTPCheck.java
@@ -0,0 +1,121 @@
+package com.liakot.easytransaction;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.concurrent.TimeUnit;
+
+public class ActivityOTPCheck extends AppCompatActivity {
+
+    EditText otp;
+    Button verifyBtn;
+    TextView resendBtn, phone;
+
+    String nameSt, phoneSt, passwordSt, verificationCode;
+    FirebaseAuth mAuth;
+    ProgressBar progressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_otp_check);
+        InitializeAll();
+
+        verifyBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                verifyBtn.setVisibility(View.GONE);
+                progressBar.setVisibility(View.VISIBLE);
+                String userInput;
+                userInput = otp.getText().toString().trim();
+
+                PhoneAuthCredential phoneAuth = PhoneAuthProvider.getCredential(verificationCode, userInput);
+
+                mAuth.signInWithCredential(phoneAuth).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            verifyBtn.setVisibility(View.GONE);
+                            progressBar.setVisibility(View.VISIBLE);
+                            //-TODO------put the value in the database
+                            Toast.makeText(ActivityOTPCheck.this, "Verification successful", Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(ActivityOTPCheck.this, MainActivity.class);
+                            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                            startActivity(intent);
+                            finish();
+                        }else{
+                            Toast.makeText(ActivityOTPCheck.this, "Verification code is not matched", Toast.LENGTH_SHORT).show();
+                            verifyBtn.setVisibility(View.VISIBLE);
+                            progressBar.setVisibility(View.GONE);
+                        }
+                    }
+                });
+            }
+        });
+        resendBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PhoneAuthProvider.getInstance().verifyPhoneNumber(phoneSt, 60, TimeUnit.SECONDS, ActivityOTPCheck.this, new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+                    @Override
+                    public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
+                        Toast.makeText(ActivityOTPCheck.this, "Verification completed", Toast.LENGTH_SHORT).show();
+                        verifyBtn.setVisibility(View.VISIBLE);
+                        progressBar.setVisibility(View.GONE);
+                    }
+
+                    @Override
+                    public void onVerificationFailed(@NonNull FirebaseException e) {
+                        Toast.makeText(ActivityOTPCheck.this, "Verification failed", Toast.LENGTH_SHORT).show();
+                        verifyBtn.setVisibility(View.VISIBLE);
+                        progressBar.setVisibility(View.GONE);
+                    }
+
+                    @Override
+                    public void onCodeSent(@NonNull String s, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+                        verificationCode = s;
+                        Toast.makeText(ActivityOTPCheck.this, "Code sent successfully", Toast.LENGTH_SHORT).show();
+                        verifyBtn.setVisibility(View.VISIBLE);
+                        progressBar.setVisibility(View.GONE);
+                    }
+                });
+            }
+        });
+    }
+
+    private void InitializeAll() {
+
+        mAuth = FirebaseAuth.getInstance();
+
+        nameSt = getIntent().getStringExtra("name");
+        phoneSt = getIntent().getStringExtra("phone");
+        passwordSt = getIntent().getStringExtra("password");
+        verificationCode = getIntent().getStringExtra("OTP");
+
+        otp = findViewById(R.id.otpCheckOTP);
+        resendBtn = findViewById(R.id.otpCheckResendBtn);
+        verifyBtn = findViewById(R.id.otpCheckVerifyBtn);
+        phone = findViewById(R.id.otpCheckPhone);
+        phone.setText(phoneSt);
+        progressBar = findViewById(R.id.otpCheckProgressBar);
+
+    }
+}
--- app/src/main/java/com/liakot/easytransaction/ActivitySignUp.java
+++ app/src/main/java/com/liakot/easytransaction/ActivitySignUp.java
@@ -1,27 +1,174 @@
 package com.liakot.easytransaction;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.concurrent.TimeUnit;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class ActivitySignUp extends AppCompatActivity {
 
     Button signUpBtn;
+    TextView signInBtn;
+    TextInputEditText name, phone, password, conPassword;
+    TextInputLayout nameLay, phoneLay, passwordLay, conPasswordLay;
+    CircleImageView logo;
+
+    ProgressBar progressBar;
+
+    FirebaseAuth newShop;
+
+    public PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallback;
+    private String verificationCode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
 
-        signUpBtn = findViewById(R.id.signUpBtn);
+        InitializeAll();
+
+
         signUpBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(ActivitySignUp.this, ActivityHome.class));
+                String nameSt, phoneSt, passwordSt, conPasswordSt;
+                nameSt = name.getText().toString();
+                phoneSt = phone.getText().toString();
+                passwordSt = password.getText().toString();
+                conPasswordSt = conPassword.getText().toString();
+
+                long number = 2000000000;
+                if (!phoneSt.isEmpty() && !phoneSt.contains("%") && !phoneSt.contains(":") && !phoneSt.contains(" ") && !phoneSt.contains(".")) {
+                    number = Long.parseLong(phoneSt);
+                }
+
+                if (nameSt.isEmpty()) {
+                    nameLay.setError("Name is empty");
+                    phoneLay.setErrorEnabled(false);
+                    passwordLay.setErrorEnabled(false);
+                    conPasswordLay.setErrorEnabled(false);
+                } else if (phoneSt.isEmpty()) {
+                    phoneLay.setError("Phone is empty");
+                    nameLay.setErrorEnabled(false);
+                    passwordLay.setErrorEnabled(false);
+                    conPasswordLay.setErrorEnabled(false);
+                } else if ((phone.length() < 10  && phone.length() > 11) || number > 1999999999 || number < 999999999) {
+                    phoneLay.setError("Invalid phone number");
+                    nameLay.setErrorEnabled(false);
+                    passwordLay.setErrorEnabled(false);
+                    conPasswordLay.setErrorEnabled(false);
+                } else if (passwordSt.isEmpty()) {
+                    passwordLay.setError("Password is empty");
+                    phoneLay.setErrorEnabled(false);
+                    nameLay.setErrorEnabled(false);
+                    conPasswordLay.setErrorEnabled(false);
+                } else if (passwordSt.length()<=6) {
+                    passwordLay.setError("Password must contain more than 6 characters");
+                    phoneLay.setErrorEnabled(false);
+                    nameLay.setErrorEnabled(false);
+                    conPasswordLay.setErrorEnabled(false);
+                } else if (!passwordSt.matches(conPasswordSt)) {
+                    conPasswordLay.setError("password and confirm password not matched");
+                    phoneLay.setErrorEnabled(false);
+                    nameLay.setErrorEnabled(false);
+                    passwordLay.setErrorEnabled(false);
+                }else{
+                    signUpBtn.setVisibility(View.GONE);
+                    progressBar.setVisibility(View.VISIBLE);
+                    nameLay.setErrorEnabled(false);
+                    phoneLay.setErrorEnabled(false);
+                    passwordLay.setErrorEnabled(false);
+                    conPasswordLay.setErrorEnabled(false);
+
+                    if(phoneSt.length() == 10){
+                        phoneSt = "+880" + phoneSt;
+                    }else{
+                        phoneSt = "+88" + phoneSt;
+                    }
+
+                    String finalPhoneSt = phoneSt;
+                    mCallback = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+                        @Override
+                        public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
+                            Toast.makeText(ActivitySignUp.this, "Verification Completed", Toast.LENGTH_SHORT).show();
+                            signUpBtn.setVisibility(View.VISIBLE);
+                            progressBar.setVisibility(View.GONE);
+                        }
+
+                        @Override
+                        public void onVerificationFailed(@NonNull FirebaseException e) {
+                            Toast.makeText(ActivitySignUp.this, "Verification failed", Toast.LENGTH_SHORT).show();
+                            signUpBtn.setVisibility(View.VISIBLE);
+                            progressBar.setVisibility(View.GONE);
+
+                        }
+
+                        @Override
+                        public void onCodeSent(@NonNull String s, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+                            verificationCode = s;
+                            Toast.makeText(ActivitySignUp.this, "Code sent successfully", Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(ActivitySignUp.this, ActivityOTPCheck.class);
+                            intent.putExtra("name", nameSt);
+                            intent.putExtra("phone", finalPhoneSt);
+                            intent.putExtra("password", passwordSt);
+                            intent.putExtra("OTP", verificationCode);
+                            startActivity(intent);
+                            finish();
+                        }
+                    };
+
+                    PhoneAuthProvider.getInstance().verifyPhoneNumber(phoneSt, 60, TimeUnit.SECONDS, ActivitySignUp.this, mCallback);
+
+                }
             }
         });
+
+        signInBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ActivitySignUp.this, ActivitySignIn.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+
+    private void InitializeAll() {
+
+        newShop = FirebaseAuth.getInstance();
+
+        signUpBtn = findViewById(R.id.signUpBtn);
+        signInBtn = findViewById(R.id.signUpSignInBtn);
+        logo = findViewById(R.id.signUpAppLogo);
+
+        name = findViewById(R.id.signUpShopName);
+        phone = findViewById(R.id.signUpPhone);
+        password = findViewById(R.id.signUpPassword);
+        conPassword = findViewById(R.id.signUpConfirmPassword);
+
+        nameLay = findViewById(R.id.signUpShopNameLayout);
+        phoneLay = findViewById(R.id.signUpPhoneLayout);
+        passwordLay = findViewById(R.id.signUpPasswordLayout);
+        conPasswordLay = findViewById(R.id.signUpConfirmPasswordLayout);
+
+        progressBar = findViewById(R.id.signUpProgressBar);
+
     }
 }
